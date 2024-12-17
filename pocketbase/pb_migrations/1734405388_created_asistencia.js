f9ce90a84af8c75cd4ceabdbc817e0fa259a334f
tostadito <ro.bruno@duocuc.cl>
2024-12-17 00:50:50 -0300

    actualizacion asis

---
+++ pocketbase/pb_migrations/1734405388_created_asistencia.js
@@ -0,0 +1,108 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = new Collection({
+    "createRule": null,
+    "deleteRule": null,
+    "fields": [
+      {
+        "autogeneratePattern": "[a-z0-9]{15}",
+        "hidden": false,
+        "id": "text3208210256",
+        "max": 15,
+        "min": 15,
+        "name": "id",
+        "pattern": "^[a-z0-9]+$",
+        "presentable": false,
+        "primaryKey": true,
+        "required": true,
+        "system": true,
+        "type": "text"
+      },
+      {
+        "cascadeDelete": false,
+        "collectionId": "pbc_2985828473",
+        "hidden": false,
+        "id": "relation1844466308",
+        "maxSelect": 1,
+        "minSelect": 0,
+        "name": "materia",
+        "presentable": false,
+        "required": false,
+        "system": false,
+        "type": "relation"
+      },
+      {
+        "cascadeDelete": false,
+        "collectionId": "ffm8odgwcsxpxqe",
+        "hidden": false,
+        "id": "relation994000813",
+        "maxSelect": 1,
+        "minSelect": 0,
+        "name": "estudiante",
+        "presentable": false,
+        "required": false,
+        "system": false,
+        "type": "relation"
+      },
+      {
+        "hidden": false,
+        "id": "date27834329",
+        "max": "",
+        "min": "",
+        "name": "fecha",
+        "presentable": false,
+        "required": false,
+        "system": false,
+        "type": "date"
+      },
+      {
+        "autogeneratePattern": "",
+        "hidden": false,
+        "id": "text643686883",
+        "max": 0,
+        "min": 0,
+        "name": "estado",
+        "pattern": "",
+        "presentable": false,
+        "primaryKey": false,
+        "required": false,
+        "system": false,
+        "type": "text"
+      },
+      {
+        "hidden": false,
+        "id": "autodate2990389176",
+        "name": "created",
+        "onCreate": true,
+        "onUpdate": false,
+        "presentable": false,
+        "system": false,
+        "type": "autodate"
+      },
+      {
+        "hidden": false,
+        "id": "autodate3332085495",
+        "name": "updated",
+        "onCreate": true,
+        "onUpdate": true,
+        "presentable": false,
+        "system": false,
+        "type": "autodate"
+      }
+    ],
+    "id": "pbc_647395869",
+    "indexes": [],
+    "listRule": null,
+    "name": "asistencia",
+    "system": false,
+    "type": "base",
+    "updateRule": null,
+    "viewRule": null
+  });
+
+  return app.save(collection);
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_647395869");
+
+  return app.delete(collection);
+})
